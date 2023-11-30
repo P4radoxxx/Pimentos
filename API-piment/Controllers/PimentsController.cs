ed51310fbc598ef1a924f590db631e3410e8bb99
Cyril <131610338+P4radoxxx@users.noreply.github.com>
2023-11-30 09:38:25 +0100

    Add files via upload

---
+++ API-piment/Controllers/PimentsController.cs
@@ -0,0 +1,70 @@
+﻿using Dapper;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+
+namespace API_piment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PimentsController : ControllerBase
+    {
+
+        #region DI
+        private readonly SqlConnection? _conn = null;
+        private readonly string str = @"Data Source=DESKTOP-FH1BO1J\SQLEXPRESS;Initial Catalog=Piments;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;Multi Subnet Failover=False";
+        public PimentsController()
+        {
+            _conn = new SqlConnection(str);
+        }
+        #endregion
+
+
+        /// <summary>
+        /// Retrieves all Piments.
+        /// </summary>
+        /// <returns>ActionResult representing the result of the operation.</returns>
+        [HttpGet]
+        [ProducesResponseType(200)] // OK
+        [ProducesResponseType(204)] // No Content
+        public ActionResult<IEnumerable<Models.Piments>> Get()
+        {
+            string sql = "SELECT * FROM [Piments]";
+            IEnumerable<Models.Piments>? items = _conn?.Query<Models.Piments>(sql).ToList();
+
+            if (items is not null)
+                return Ok(items); 
+
+            return NoContent(); 
+        }
+
+        /// <summary>
+        /// Retrieves a specific Piment by its ID.
+        /// </summary>
+        /// <param name="id">The ID of the Piment to retrieve.</param>
+        /// <returns>ActionResult representing the result of the operation.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(200)] // OK
+        [ProducesResponseType(204)] // No Content
+        public ActionResult<Models.Piments>? Get(int id)
+        {
+            try
+            {
+                string sql = "SELECT * FROM Piments WHERE Id = @idP";
+                Models.Piments? item = _conn?.QuerySingle<Models.Piments>(sql, new { idP = id });
+
+                if (item is not null)
+                    return Ok(item);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw ex;
+            }
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ API-piment/Models/User.cs
@@ -0,0 +1,56 @@
+﻿namespace API_piment.Models
+{
+    public record class User
+    {
+        public int Id { get;  }
+        public string Name { get;} = string.Empty;
+        public string Email { get; } = string.Empty;
+        public string Password { get; } = string.Empty;
+        public string Role { get;  } = string.Empty;
+
+        public User(string name, string email, string password, string role)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+            this.Role = role;
+        }
+    }
+
+    public record class UserRegisterDTO 
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public record class UserLogDTO 
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public record class UserTokenDTO 
+    {
+        public int Id { get; }
+        public string Role { get; set; } = string.Empty;
+    }
+
+    public record class UserDisplayDTO
+    {
+        public int Id { get; }
+        public string Name { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+
+
+
+    public record class UserAdminInfo
+    {
+        public int Id { get; }
+        public string Name { get; } = string.Empty;
+        public string Email { get; } = string.Empty;
+        public string Password { get; } = string.Empty;
+        public string Role { get; } = string.Empty;
+    }
+}
